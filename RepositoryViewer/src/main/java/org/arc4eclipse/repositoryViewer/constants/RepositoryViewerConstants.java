9e06bdb04f7b13e7bd80d1b201e758db39545588
Phil <phil.rice@iee.org>
2011-07-21 14:24:17 +0100

    Remove repository viewer project

--- RepositoryViewer/src/main/java/org/arc4eclipse/repositoryViewer/constants/RepositoryViewerConstants.java
+++
@@ -1,7 +0,0 @@
-package org.arc4eclipse.repositoryViewer.constants;
-
-public class RepositoryViewerConstants {
-
-	public final static String defaultHost = "localhost";
-	public final static int defaultPort = 8888;
-}
