7b704c4226cbb355b618932b9dc0e8da3dda7b2c
Phil <phil.rice@iee.org>
2011-09-12 17:13:50 +0100

    Ensuring plugins ok

--- EntityJarData/src/main/java/org/softwareFm/entityJarSummaryData/JarConfigurer.java
+++ EntityJarData/src/main/java/org/softwareFm/entityJarSummaryData/JarConfigurer.java
@@ -2,13 +2,14 @@ package org.softwareFm.entityJarSummaryData;
 
 import org.softwareFm.core.plugin.AbstractDisplayContainerFactoryConfigurer;
 import org.softwareFm.displayCore.api.IDisplayContainerFactory;
+import org.softwareFm.softwareFmImages.artifacts.ArtifactsAnchor;
 
 public class JarConfigurer extends AbstractDisplayContainerFactoryConfigurer {
 
 	@Override
 	public void configure(IDisplayContainerFactory factory) {
 		factory.register(makeMap("jar", "jarData", "jar", "artifact.jar"));
-		// factory.register(makeMapWithView("jar", "project.url", "composite", ArtifactsAnchor.projectKey, "project"));
+		factory.register(makeMapWithView("jar", "project.url", "composite", ArtifactsAnchor.projectKey, "project"));
 	}
 
 }
