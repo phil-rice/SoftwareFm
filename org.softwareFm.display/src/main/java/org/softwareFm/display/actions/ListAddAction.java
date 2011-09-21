99cb01f0be4d68ad10718e01944aed75bad31849
Phil <phil.rice@iee.org>
2011-09-21 18:09:17 +0100

    Adding to lists now a little nicer

--- org.softwareFm.display/src/main/java/org/softwareFm/display/actions/ListAddAction.java
+++ org.softwareFm.display/src/main/java/org/softwareFm/display/actions/ListAddAction.java
@@ -5,20 +5,25 @@ import java.util.List;
 import org.softwareFm.display.data.ActionData;
 import org.softwareFm.display.displayer.DisplayerDefn;
 import org.softwareFm.display.displayer.IDisplayer;
+import org.softwareFm.utilities.callbacks.ICallback;
 import org.softwareFm.utilities.collections.Lists;
+import org.softwareFm.utilities.strings.Strings;
 
 public class ListAddAction implements IAction {
 
 	@Override
 	@SuppressWarnings("unchecked")
-	public void execute(ActionContext actionContext, DisplayerDefn displayerDefn, IDisplayer displayer, int index, ActionData actionData) {
-		String key = displayerDefn.dataKey;
-		List<String> currentList =Lists.nullSafe((List<String>) actionContext.dataGetter.getDataFor(key));
-		if (currentList.size()>0 && currentList.get(currentList.size()-1).trim().length()==0)
-			return;
-		List<String> newList = Lists.append(currentList, " ");
-		actionContext.updateStore.update(actionData, key, newList.toArray(new String[0]));
-		System.out.println("Adding to: "+index +","+ actionData.formalParams+", " +actionData. actualParams +"\n...." + newList);
+	public void execute(final ActionContext actionContext, DisplayerDefn displayerDefn, IDisplayer displayer, int index, final ActionData actionData) {
+		final String key = displayerDefn.dataKey;
+		final List<String> currentList =Lists.nullSafe((List<String>) actionContext.dataGetter.getDataFor(key));
+		actionContext.editorFactory.displayEditor(displayer.getButtonComposite().getShell(), "editor.text", displayerDefn, actionContext, actionData, new ICallback<Object>() {
+			@Override
+			public void process(Object t) throws Exception {
+				String string = Strings.nullSafeToString(t);
+				List<String> newList = Lists.append(currentList, string);
+				actionContext.updateStore.update(actionData, key, newList.toArray(new String[0]));
+			}
+		}, "");
 	}
 
 }
