59a0d015038f7a837ef8608d177989573768c363
Phil <phil.rice@iee.org>
2012-01-23 13:27:09 +0000

    Moving to git://git.softwarefm.com:80

--- org.softwareFm.explorer.eclipse/src/main/java/org/softwareFm/explorer/eclipse/EclipseConstants.java
+++ org.softwareFm.explorer.eclipse/src/main/java/org/softwareFm/explorer/eclipse/EclipseConstants.java
@@ -3,6 +3,6 @@ package org.softwareFm.explorer.eclipse;
 public class EclipseConstants {
 
 	public static final String softwareFmServerUrl = "www.softwarefm.com";
-	public static final String gitProtocolAndGitServerName = "git://git.softwarefm.com/";
+	public static final String gitProtocolAndGitServerName = "git://git.softwarefm.com:80/";
 
 }
